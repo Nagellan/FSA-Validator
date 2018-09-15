e58db7932af2650bca64cd5d1ea8fbdc604be187
Nagellan <31626879+Nagellan@users.noreply.github.com>
2018-09-15 14:36:21 +0300

    Add basic skeleton for State class

--- src/State.java
+++ src/State.java
@@ -1,2 +1,14 @@
+import javafx.util.Pair;
+import java.util.LinkedList;
+
+/**
+ * This class represents a state in FSA
+ * Each state has a set of transitions represented as pairs of letter and state connected by it with the original one.
+ */
 public class State {
+    public State() {
+        this.trans = new LinkedList<>();
+    }
+
+    private LinkedList<Pair<String, State>> trans;
 }
